332c9f55f300a4d0bc868b1772ad8de28062a91a
nhavuong <nhatv@uci.edu>
2021-10-10 12:34:22 -0700

    basic GUI

---
+++ load.py
@@ -0,0 +1,17 @@
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+import sys
+
+class UI(QMainWindow):
+    def __init__(self):
+        super(UI, self).__init__()
+
+        uic.loadUi("current_surgical.ui", self)
+
+        self.show()
+
+
+
+app = QApplication(sys.argv)
+UIWindow = UI()
+app.exec_()
