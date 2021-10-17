de45da023eeb6f1c7185ea799796514e870f4d4c
Shaun <hengxuan.dong@gmail.com>
2021-10-17 13:46:55 -0700

    code to connect

--- load.py
+++ load.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic
 import sys
 
+
 class UI(QMainWindow):
     def __init__(self):
         super(UI, self).__init__()
@@ -9,7 +10,6 @@ class UI(QMainWindow):
         uic.loadUi("current_surgical.ui", self)
 
         self.show()
-
 
 
 app = QApplication(sys.argv)
