2b0edec1facf020a3de948a66557d2726d663cc1
TssT96 <42284547+TssT96@users.noreply.github.com>
2021-11-10 01:43:56 -0800

    Zitian-dev qt first window delete 2d slice update 2d slice trackpad zoom in&out function update code combination(text and render window move to class objects) 11/10/21

--- Code/VTKrenderwindow.py
+++ Code/VTKrenderwindow.py
@@ -21,26 +21,9 @@ class RenderWindow(Qt.QMainWindow):
         # logo
         self.setWindowIcon(QtGui.QIcon('..\data\logo1.png'))
 
-        # while(True):
-        #     position = self.vivecontrol.devices["controller_1"].get_pose_euler(
-        #     )
-
-        #     start = time.time()
-        #     message = ""
-        #     if not hasattr(position, '__iter__'):
-        #         message = "Waiting for controller."
-        #         print("\r" + message, end="")
-        #         sleep_time = 1/self.REFRESH_RATE-(time.time()-start)
-        #         if sleep_time > 0:
-        #             time.sleep(sleep_time)
-        #     else:
-        #         print("\r" + "Start to initial VTK window.", end="")
-        #         break
-
         print("VTK Render Window Start")
         # setup Qt frame
         self.frame = Qt.QFrame()
-        self.frame.setMinimumSize(1090, 800)
         self.vl = Qt.QVBoxLayout()
         self.vtkWidget = QVTKRenderWindowInteractor(self.frame)
         self.vl.addWidget(self.vtkWidget)
@@ -61,12 +44,6 @@ class RenderWindow(Qt.QMainWindow):
         self.iren.AddObserver("TimerEvent", self.callback_func)
         self.iren.SetRenderWindow(self.rw)
 
-        # Define viewport ranges.
-        xmins = [0, .5]
-        xmaxs = [0.5, 1]
-        ymins = [0, 0.5]
-        ymaxs = [1, 1]
-
         # get sources
         sources = self.get_sources(ct_file, stl_file)
         # create tumor actor to be added later on
@@ -79,48 +56,19 @@ class RenderWindow(Qt.QMainWindow):
 
         # render main screen
         print("render main screen")
-        main_ren = vtk.vtkRenderer()
+        main_ren = self.vtkRender(0)
         self.rw.AddRenderer(main_ren)
-        main_ren.SetViewport(xmins[0], ymins[0], xmaxs[0], ymaxs[0])
 
         self.camera = main_ren.GetActiveCamera()
         # self.camera.Azimuth(30)
         # self.camera.Elevation(30)
 
-        color_transfer_function = vtk.vtkColorTransferFunction()
-        color_transfer_function.AddRGBPoint(-3024, 0, 0, 0, 0.5, 0.0)
-        color_transfer_function.AddRGBPoint(-16,
-                                            0.73, 0.25, 0.30, 0.49, .61)
-        color_transfer_function.AddRGBPoint(
-            641, .90, .82, .56, .5, 0.0)
-        color_transfer_function.AddRGBPoint(
-            3070, 1.0, 1.0, 1.0, .5, 0.0)
-        color_transfer_function.AddRGBPoint(
-            3071, 0.0, .333, 1.0, .5, 0.0)
-
-        opacity_transfer_function = vtk.vtkPiecewiseFunction()
-        opacity_transfer_function.AddPoint(-3024, 0, 0.5, 0.0)
-        opacity_transfer_function.AddPoint(-16, 0, .49, .61)
-        opacity_transfer_function.AddPoint(641, .72, .5, 0.0)
-        opacity_transfer_function.AddPoint(3071, .71, 0.5, 0.0)
-
         volMapper = vtk.vtkGPUVolumeRayCastMapper()
         volMapper.SetInputConnection(sources[0].GetOutputPort())
 
-        volume_property = vtk.vtkVolumeProperty()
-        volume_property.SetColor(color_transfer_function)
-        volume_property.SetScalarOpacity(opacity_transfer_function)
-        volume_property.SetInterpolationTypeToLinear()
-        volume_property.ShadeOn()
-        volume_property.SetAmbient(0.1)
-        volume_property.SetDiffuse(0.9)
-        volume_property.SetSpecular(0.2)
-        volume_property.SetSpecularPower(10.0)
-        volume_property.SetScalarOpacityUnitDistance(0.8919)
-
         volume = vtk.vtkVolume()
         volume.SetMapper(volMapper)
-        volume.SetProperty(volume_property)
+        volume.SetProperty(self.vtkVolume())
         volume.RotateX(-90)
         main_ren.AddVolume(volume)
         main_ren.AddActor(self.needle_actor)
@@ -142,39 +90,33 @@ class RenderWindow(Qt.QMainWindow):
         # main_ren.SetViewport(xmins[0], ymins[0], xmaxs[0], ymaxs[0])
         # main_ren.AddViewProp(imageActor)
 
-        # text
-        txt = vtk.vtkTextActor()
-        txt.SetInput('Patient name / info')
-        txtprop = txt.GetTextProperty()
-        txtprop.SetFontFamilyToArial()
-        txtprop.BoldOn()
-        txtprop.SetFontSize(20)
-        txtprop.ShadowOn()
-        txtprop.SetShadowOffset(4, 4)
-        txtprop.SetColor(vtkNamedColors().GetColor3d('Cornsilk'))
-        txt.SetDisplayPosition(10, 700)
-        main_ren.AddActor(txt)
+        main_ren.AddActor(self.txtActor(1, 1, 15, 'Patient name: Hengxuan'))
 
         main_ren.ResetCamera()
         main_ren.ResetCameraClippingRange()
 
         # render side window
         print("render side screen 1")
-        side_ren1 = vtk.vtkRenderer()
+        side_ren1 = self.vtkRender(1)
         self.rw.AddRenderer(side_ren1)
-        side_ren1.SetViewport(xmins[1], ymins[1], xmaxs[1], ymaxs[1])
+        # side_ren1.SetViewport(xmins[1], ymins[1], xmaxs[1], ymaxs[1])
         side_ren1.SetActiveCamera(self.camera)
         side_ren1.AddActor(tumor_actor)
 
         side_ren1.ResetCamera()
         side_ren1.ResetCameraClippingRange()
 
+        side_ren2 = self.vtkRender(2)
+        (sactor, self.reslice) = self.slice(sources[0])
+        side_ren2.AddActor(sactor)
+        self.rw.AddRenderer(side_ren2)
+
         self.frame.setLayout(self.vl)
         self.setCentralWidget(self.frame)
         cam_pos_x, cam_pos_y, cam_pos_z = self.camera.GetPosition()
         # self.camera.SetFocalPoint(0, -500, 200)
         self.camera.SetPosition(cam_pos_x, cam_pos_y, 1700)
-        self.showMaximized()
+        self.show()
         self.rw.Render()
         self.iren.Start()
 
@@ -222,27 +164,30 @@ class RenderWindow(Qt.QMainWindow):
 
             # Rotation about axises on the trackpad pression
             track_pad_border = 0.1
-            if(controller_status['trackpad_touched'] == True and controller_status['trackpad_pressed'] == True):
-                if abs(controller_status['trackpad_x']) > track_pad_border and abs(controller_status['trackpad_y']) > track_pad_border:
+            if controller_status['trackpad_touched'] == True and controller_status['trackpad_pressed'] == True:
+                if abs(controller_status['trackpad_x']) > track_pad_border and abs(
+                        controller_status['trackpad_y']) > track_pad_border:
                     self.camera.Azimuth(
                         3 * self.return_sign(controller_status['trackpad_x']))
                     self.camera.Elevation(
                         3 * self.return_sign(controller_status['trackpad_y']))
-                elif abs(controller_status['trackpad_x']) < track_pad_border and abs(controller_status['trackpad_y']) > track_pad_border:
+                elif abs(controller_status['trackpad_x']) < track_pad_border < abs(
+                        controller_status['trackpad_y']):
                     self.camera.Elevation(
                         3 * self.return_sign(controller_status['trackpad_y']))
-                elif abs(controller_status['trackpad_x']) > track_pad_border and abs(controller_status['trackpad_y']) < track_pad_border:
+                elif abs(controller_status['trackpad_x']) > track_pad_border > abs(
+                        controller_status['trackpad_y']):
                     self.camera.Azimuth(
                         3 * self.return_sign(controller_status['trackpad_x']))
 
             # since the pressure on the trigger is from 0 to 1,
             # I decided compute the trigger strength use 1 - controller_status['trigger']
             # change the color of the needle from (1, 1, 1) to (1, 1 * trigger strength, )
-            if(controller_status['trigger'] > 0.1):
+            if controller_status['trigger'] > 0.1:
                 print(controller_status)
-                trigger_strength = 1-controller_status['trigger']
+                trigger_strength = 1 - controller_status['trigger']
                 self.needle_actor.GetProperty().SetColor(
-                    [1, 1*trigger_strength, 1*trigger_strength])
+                    [1, 1 * trigger_strength, 1 * trigger_strength])
             else:
                 # when release the trigger, change the color of needle to white.
                 self.needle_actor.GetProperty().SetColor([1, 1, 1])
@@ -250,6 +195,26 @@ class RenderWindow(Qt.QMainWindow):
                                               * 700 - 400, position[2] * -300)
                 self.needle_actor.SetOrientation(
                     -position[5], -position[4], -position[3])
+
+            if controller_status['grip_button']:
+                pY = controller_status['trackpad_y']
+                print(pY)
+
+                if pY != 0:
+                    self.reslice.Update()
+                    matrix = self.reslice.GetResliceAxes()
+                    # move the center point that we are slicing through
+                    center = matrix.MultiplyPoint((0, 0, pY, 1))
+                    print(center)
+                    if -194 > center[0]:
+                        matrix.SetElement(0, 3, -193)
+                    elif 194 < center[0]:
+                        matrix.SetElement(0, 3, 193)
+                    else:
+                        matrix.SetElement(0, 3, center[0])
+
+                    matrix.SetElement(1, 3, center[1])
+                    matrix.SetElement(2, 3, center[2])
 
         self.rw.Render()
 
@@ -270,21 +235,132 @@ class RenderWindow(Qt.QMainWindow):
         needle_actor.GetProperty().SetInterpolationToPhong()
         needle_actor.GetProperty().SetRepresentationToSurface()
 
-        # sample needle
-        # cylinder = vtk.vtkCylinderSource()
-        # cylinder.SetCenter(0.0, 0.0, 0.0)
-        # cylinder.Update()
-        #
-        # cylinder_mapper = vtk.vtkPolyDataMapper()
-        # cylinder_mapper.SetInputConnection(cylinder.GetOutputPort())
-        # c_actor = vtk.vtkActor()
-        # c_actor.SetMapper(cylinder_mapper)
-        #
-        # c_actor.SetPosition(0, 0, 0)
-        # c_actor.GetProperty().SetColor(vtkNamedColors().GetColor3d('Goldenrod'))
-        # c_actor.GetProperty().SetOpacity(1)
-        # c_actor.GetProperty().SetInterpolationToPhong()
-        # c_actor.GetProperty().SetRepresentationToSurface()
-        # c_actor.RotateY(30)
-
         return needle_actor
+
+    def vtkColor(self):
+        color_transfer = vtk.vtkColorTransferFunction()
+        color_transfer.AddRGBPoint(-3024, 0, 0, 0, 0.5, 0.0)
+        color_transfer.AddRGBPoint(-16, 0.73, 0.25, 0.30, 0.49, .61)
+        color_transfer.AddRGBPoint(641, .90, .82, .56, .5, 0.0)
+        color_transfer.AddRGBPoint(3070, 1.0, 1.0, 1.0, .5, 0.0)
+        color_transfer.AddRGBPoint(3071, 0.0, .333, 1.0, .5, 0.0)
+
+        return color_transfer
+
+    def vtkOpacity(self):
+        opacity_transfer = vtk.vtkPiecewiseFunction()
+        opacity_transfer.AddPoint(-3024, 0, 0.5, 0.0)
+        opacity_transfer.AddPoint(-16, 0, .49, .61)
+        opacity_transfer.AddPoint(641, .72, .5, 0.0)
+        opacity_transfer.AddPoint(3071, .71, 0.5, 0.0)
+
+        return opacity_transfer
+
+    def vtkVolume(self):
+        volume_property = vtk.vtkVolumeProperty()
+        volume_property.SetColor(self.vtkColor())
+        volume_property.SetScalarOpacity(self.vtkOpacity())
+        volume_property.SetInterpolationTypeToLinear()
+        volume_property.ShadeOn()
+        volume_property.SetAmbient(0.1)
+        volume_property.SetDiffuse(0.9)
+        volume_property.SetSpecular(0.2)
+        volume_property.SetSpecularPower(10.0)
+        volume_property.SetScalarOpacityUnitDistance(0.8919)
+
+        return volume_property
+
+    def vtkRender(self, pos):
+        # Define viewport ranges.
+        xmins = [0, .5, 0.5]
+        xmaxs = [0.5, 1, 1]
+        ymins = [0, 0.5, 0]
+        ymaxs = [1, 1, 0.5]
+
+        ren = vtk.vtkRenderer()
+        ren.SetViewport(xmins[pos], ymins[pos], xmaxs[pos], ymaxs[pos])
+
+        return ren
+
+    def txtActor(self, Xpos, Ypos, Fontsize, text):
+        txt = vtk.vtkTextActor()
+        txt.SetInput(text)
+        txtprop = txt.GetTextProperty()
+        txtprop.SetFontFamilyToArial()
+        txtprop.BoldOn()
+        txtprop.SetFontSize(Fontsize)
+        txtprop.ShadowOn()
+        txtprop.SetShadowOffset(4, 4)
+        txtprop.SetColor(vtkNamedColors().GetColor3d('Cornsilk'))
+        txt.SetPosition(Xpos, Ypos)
+
+        return txt
+
+    def slice(self, source):
+        ctsource = source
+        ctsource.SetDataExtent(0, 63, 0, 63, 1, 93)
+        ctsource.SetDataSpacing(3.2, 3.2, 1.5)
+        ctsource.SetDataOrigin(0.0, 0.0, 0.0)
+        ctsource.SetDataScalarTypeToUnsignedShort()
+        ctsource.UpdateWholeExtent()
+
+        # Calculate the center of the volume
+        ctsource.Update()
+        (xMin, xMax, yMin, yMax, zMin, zMax) = ctsource.GetExecutive().GetWholeExtent(ctsource.GetOutputInformation(0))
+        (xSpacing, ySpacing, zSpacing) = ctsource.GetOutput().GetSpacing()
+        (x0, y0, z0) = ctsource.GetOutput().GetOrigin()
+
+        center = [x0 + xSpacing * 0.5 * (xMin + xMax),
+                  y0 + ySpacing * 0.5 * (yMin + yMax),
+                  z0 + zSpacing * 0.5 * (zMin + zMax)]
+
+        # Matrices for axial, coronal, sagittal, oblique view orientations
+        axial = vtk.vtkMatrix4x4()
+        axial.DeepCopy((1, 0, 0, center[0],
+                        0, 1, 0, center[1],
+                        0, 0, 1, center[2],
+                        0, 0, 0, 1))
+
+        coronal = vtk.vtkMatrix4x4()
+        coronal.DeepCopy((1, 0, 0, center[0],
+                          0, 0, 1, center[1],
+                          0, -1, 0, center[2],
+                          0, 0, 0, 1))
+
+        sagittal = vtk.vtkMatrix4x4()
+        sagittal.DeepCopy((0, 0, -1, center[0],
+                           1, 0, 0, center[1],
+                           0, -1, 0, center[2],
+                           0, 0, 0, 1))
+
+        oblique = vtk.vtkMatrix4x4()
+        oblique.DeepCopy((1, 0, 0, center[0],
+                          0, 0.866025, -0.5, center[1],
+                          0, 0.5, 0.866025, center[2],
+                          0, 0, 0, 1))
+
+        # Extract a slice in the desired orientation
+        reslice = vtk.vtkImageReslice()
+        reslice.SetInputConnection(ctsource.GetOutputPort())
+        reslice.SetOutputDimensionality(2)
+        reslice.SetResliceAxes(sagittal)
+        reslice.SetInterpolationModeToLinear()
+
+        # Create a greyscale lookup table
+        table = vtk.vtkLookupTable()
+        table.SetRange(0, 2000)  # image intensity range
+        table.SetValueRange(0.0, 1.0)  # from black to white
+        table.SetSaturationRange(0.0, 0.0)  # no color saturation
+        table.SetRampToLinear()
+        table.Build()
+
+        # Map the image through the lookup table
+        color = vtk.vtkImageMapToColors()
+        color.SetLookupTable(table)
+        color.SetInputConnection(reslice.GetOutputPort())
+
+        actor = vtk.vtkImageActor()
+        actor.GetMapper().SetInputConnection(color.GetOutputPort())
+
+        return actor, reslice
+
--- Code/main.py
+++ Code/main.py
@@ -17,65 +17,62 @@ class Window(QMainWindow):
         self.curdir = os.path.dirname(__file__)
         self.ct_path = os.path.join(self.curdir, '../data/volume-105.nhdr')
         self.stl_path = os.path.join(self.curdir, '../data/Liver.stl')
-        self.title = "File upload window"
-        self.top = 100
-        self.left = 100
-        self.width = 680
-        self.height = 500
+        self.title = "VTK Render Window"
 
-        self.BrowseButton_ct = QPushButton("Browse CT File", self)
-        self.BrowseButton_ct.move(200, 175)
-        self.BrowseButton_ct.setToolTip(
-            "<h4>Browse file you want to up load</h4>")
-        self.BrowseButton_ct.clicked.connect(self.ct_browseButton_handler)
+        # self.BrowseButton_ct = QPushButton("Browse CT File", self)
+        # self.BrowseButton_ct.move(200, 175)
+        # self.BrowseButton_ct.setToolTip(
+        #     "<h4>Browse file you want to up load</h4>")
+        # self.BrowseButton_ct.clicked.connect(self.ct_browseButton_handler)
+        #
+        # self.BrowseButton_stl = QPushButton("Browse stl File", self)
+        # self.BrowseButton_stl.move(350, 175)
+        # self.BrowseButton_stl.setToolTip(
+        #     "<h4>Browse file you want to up load</h4>")
+        # self.BrowseButton_stl.clicked.connect(self.stl_browseButton_handler)
+        #
+        # self.pushButton = QPushButton("Upload", self)
+        # self.pushButton.move(275, 250)
+        # self.pushButton.setToolTip("<h3>Start the Session</h3>")
+        #
+        # self.pushButton.clicked.connect(self.window2_button_handler)
 
-        self.BrowseButton_stl = QPushButton("Browse stl File", self)
-        self.BrowseButton_stl.move(350, 175)
-        self.BrowseButton_stl.setToolTip(
-            "<h4>Browse file you want to up load</h4>")
-        self.BrowseButton_stl.clicked.connect(self.stl_browseButton_handler)
+        self.window2(self.ct_path, self.stl_path)
+        # self.main_window()
 
-        self.pushButton = QPushButton("Upload", self)
-        self.pushButton.move(275, 250)
-        self.pushButton.setToolTip("<h3>Start the Session</h3>")
-
-        self.pushButton.clicked.connect(self.window2_button_handler)
-
-        self.main_window()
-
-    def main_window(self):
-        # self.label = QLabel("Here is file direction", self)
-        # self.label.move(285, 175)
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.top, self.left, self.width, self.height)
-        self.show()
+    # def main_window(self):
+    #     # self.label = QLabel("Here is file direction", self)
+    #     # self.label.move(285, 175)
+    #     self.setWindowTitle(self.title)
+    #     self.setGeometry(self.top, self.left, self.width, self.height)
+    #     self.show()
 
     def window2(self, ct_file, stl_file):
         print("window2" + ct_file + stl_file)
         self.w = VTKrenderwindow.RenderWindow(ct_file, stl_file)
-        self.w.show()
-        self.hide()
+        self.w.showMaximized()
+        # self.hide()
 
-    def window2_button_handler(self):
-        if len(self.ct_path) != 0 and len(self.stl_path) != 0:
-            print("handler" + self.ct_path + self.stl_path)
-            self.window2(self.ct_path, self.stl_path)
+    # def window2_button_handler(self):
+    #     if len(self.ct_path) != 0 and len(self.stl_path) != 0:
+    #         print("handler" + self.ct_path + self.stl_path)
+    #         self.window2(self.ct_path, self.stl_path)
+    #
+    # def ct_browseButton_handler(self):
+    #     print("Browse Button pressed")
+    #     self.open_dialog_box(1)
+    #
+    # def stl_browseButton_handler(self):
+    #     print("Browse Button pressed")
+    #     self.open_dialog_box(2)
 
-    def ct_browseButton_handler(self):
-        print("Browse Button pressed")
-        self.open_dialog_box(1)
-
-    def stl_browseButton_handler(self):
-        print("Browse Button pressed")
-        self.open_dialog_box(2)
-
-    def open_dialog_box(self, flag):
-        filename = QFileDialog.getOpenFileName()
-        if flag == 1:
-            self.ct_path = filename[0]
-        elif flag == 2:
-            self.stl_path = filename[0]
-        print(filename)
+    # def open_dialog_box(self, flag):
+    #     filename = QFileDialog.getOpenFileName()
+    #     if flag == 1:
+    #         self.ct_path = filename[0]
+    #     elif flag == 2:
+    #         self.stl_path = filename[0]
+    #     print(filename)
 
 
 if __name__ == "__main__":
